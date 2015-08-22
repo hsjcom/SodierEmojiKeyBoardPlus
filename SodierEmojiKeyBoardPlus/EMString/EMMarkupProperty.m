5919a860f18930169aaf5354e26c63b18207529f
hsjcom <hongsjcn@qq.com>
2015-08-22 14:12:16 +0800

    add EMString,rich text

---
+++ SodierEmojiKeyBoardPlus/EMString/EMMarkupProperty.m
@@ -0,0 +1,46 @@
+//
+//  EMMarkupProperty.m
+//  EMString
+//
+//  Created by Tanguy Aladenise on 2014-11-28.
+//  Copyright (c) 2014 Tanguy Aladenise. All rights reserved.
+//
+
+#import "EMMarkupProperty.h"
+
+@implementation EMMarkupProperty
+
+#pragma mark - Static
+
+NSString * const kEMDefaultMarkup      = @"<defaultEM>";
+NSString * const kEMDefaultCloseMarkup = @"</defaultEM>";
+
+NSString * const kEMParagraphMarkup      = @"<p>";
+NSString * const kEMParagraphCloseMarkup = @"</p>";
+
+NSString * const kEMStrongMarkup         = @"<strong>";
+
+NSString * const kEMEmphasisMarkup       = @"<em>";
+
+NSString * const kEMUnderlineMarkup      = @"<u>";
+
+NSString * const kEMStrikethroughMarkup      = @"<s>";
+
+
+NSString * const kEMH1Markup      = @"<h1>";
+
+NSString * const kEMH2Markup      = @"<h2>";
+
+NSString * const kEMH3Markup      = @"<h3>";
+
+NSString * const kEMH4Markup      = @"<h4>";
+
+NSString * const kEMH5Markup      = @"<h5>";
+
+NSString * const kEMH6Markup      = @"<h6>";
+
+
+@end
+// 版权属于原作者
+// http://code4app.com (cn) http://code4app.net (en)
+// 发布代码于最专业的源码分享网站: Code4App.com
--- SodierEmojiKeyBoardPlus/ViewController.m
+++ SodierEmojiKeyBoardPlus/ViewController.m
@@ -8,6 +8,9 @@
 
 #import "ViewController.h"
 
+#import "NSString+EMAdditions.h"
+#import "EMStringStylingConfiguration.h"
+
 @interface ViewController ()
 
 @end
@@ -65,6 +68,11 @@
     
     [self constructView];
     [self constructKeyBoardView];
+    
+//    [self attributedString];
+//    [self textString];
+    
+    [self richText];
 }
 
 - (void)constructView{
@@ -250,10 +258,6 @@
 
     
     _contentLabel.text = textView.text;
-    
-//    [self attributedString];
-    
-//    [self textString];
 }
 
 //NSMutableAttributedString
@@ -328,6 +332,50 @@
 //                              constrainedToSize:CGSizeMake(self.view.frame.size.width - 20, CGFLOAT_MAX)
 //                                  lineBreakMode:NSLineBreakByTruncatingTail];
 //    _contentLabel.frame = CGRectMake(_contentLabel.frame.origin.x, _contentLabel.frame.origin.y, _contentLabel.frame.size.width, textSize.height);
+}
+
+//富文本
+- (void)richText {
+    [EMStringStylingConfiguration sharedInstance].defaultFont  = [UIFont fontWithName:@"Avenir-Light" size:15];
+    [EMStringStylingConfiguration sharedInstance].strongFont   = [UIFont fontWithName:@"Avenir" size:15];
+    [EMStringStylingConfiguration sharedInstance].emphasisFont = [UIFont fontWithName:@"Avenir-LightOblique" size:15];
+    
+    
+    // Then for the demo I created a bunch of custom styling class to provide examples
+    
+    // The code tag a little bit like in Github (custom font, custom color, a background color)
+    EMStylingClass *aStylingClass = [[EMStylingClass alloc] initWithMarkup:@"<code>"];
+    aStylingClass.color           = [UIColor colorWithWhite:0.151 alpha:1.000];
+    aStylingClass.font            = [UIFont fontWithName:@"Courier" size:14];
+    aStylingClass.attributes      = @{NSBackgroundColorAttributeName : [UIColor colorWithWhite:0.901 alpha:1.000]};
+    [[EMStringStylingConfiguration sharedInstance] addNewStylingClass:aStylingClass];
+    
+    // A styling class for text in RED
+    aStylingClass       = [[EMStylingClass alloc] initWithMarkup:@"<red>"];
+    aStylingClass.color = [UIColor colorWithRed:0.773 green:0.000 blue:0.263 alpha:1.000];
+    [[EMStringStylingConfiguration sharedInstance] addNewStylingClass:aStylingClass];
+    
+    // A styling class for text in GREEN
+    aStylingClass       = [[EMStylingClass alloc] initWithMarkup:@"<green>"];
+    aStylingClass.color = [UIColor colorWithRed:0.269 green:0.828 blue:0.392 alpha:1.000];
+    [[EMStringStylingConfiguration sharedInstance] addNewStylingClass:aStylingClass];
+    
+    // A styling class for text with stroke
+    aStylingClass            = [[EMStylingClass alloc] initWithMarkup:@"<stroke>"];
+    aStylingClass.font       = [UIFont fontWithName:@"Avenir-Black" size:26];
+    aStylingClass.color      = [UIColor whiteColor];
+    aStylingClass.attributes = @{NSStrokeWidthAttributeName: @-6,
+                                 NSStrokeColorAttributeName:[UIColor colorWithRed:0.111 green:0.568 blue:0.764 alpha:1.000]};
+    [[EMStringStylingConfiguration sharedInstance] addNewStylingClass:aStylingClass];
+    
+    /**
+     *  END of setup to be done only once througout entire app.
+     */
+    
+    // Our big text stored in a string with tags for EMString styling
+    NSString *text = @"<h4>About EMString</h4>\n[微笑][大兵][微笑][微笑][微笑][大兵][微笑][微笑]<p><strong>EMString</strong> stands for <em><strong>E</strong>asy <strong>M</strong>arkup <strong>S</strong>tring</em>. I hesitated to call it SONSAString : Sick Of NSAttributedString...</p>\n<p>Most of the time if you need to display a text with different styling in it, like \"<strong>This text is bold</strong> but then <em>italic.</em>\", you would use an <code>NSAttributedString</code> and its API. Same thing if suddenly your text is <green><strong>GREEN</strong></green> and then <red><strong>RED</strong></red>...</p><p>Personnaly I don't like it! It clusters my classes with a lot of boilerplate code to find range and apply style, etc...</p>\n<p>This is what <strong>EMString</strong> is about. Use the efficient <u>HTML markup</u> system we all know and get an iOS string stylized in one line of code and behave like you would expect it to.</p>\n<h1>h1 header</h1><h2>h2 header</h2><h3>h3 header</h3><stroke>Stroke text</stroke>\n<strong>strong</strong>\n<em>emphasis</em>\n<u>underline</u>\n<s>strikethrough</s>\n<code>and pretty much whatever you think of...</code>";
+    
+    _contentLabel.text = text.attributedString;
 }
 
 @end
